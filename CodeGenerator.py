c0960907b434ff47d06e2e8e80ded568a532cd27
Victor Dominguez <vdominguez1993@gmail.com>
2019-10-22 21:52:12 +0200

    In actions during, entry and exit parsed properly

--- CodeGenerator.py
+++ CodeGenerator.py
@@ -44,7 +44,7 @@ class PlantUMLCodeGeneration():
             raise Exception('File {} does not exist.'.format(plantuml_file))
 
     def CheckUml(self):
-        if subprocess.call(['plantuml',self.plantuml_file, '--checkonly']) == 0:
+        if subprocess.call(['plantuml', '-checkonly', self.plantuml_file]) == 0:
             return True
         else:
             return False
@@ -65,7 +65,7 @@ class PlantUMLCodeGeneration():
 
     def ParseStateMachine(self):
         uml = self.GetUMLText()
-        uml_params = self.ParseStateMachineAsDict()
+        uml_params = self.ParseStateMachineAsDict(uml_text = self.GetUMLText(grouped=True))[0]
         return uml, uml_params
 
     def GetUMLText(self, grouped = False):
@@ -81,6 +81,9 @@ class PlantUMLCodeGeneration():
                     #First strip the line to forget about leading and trailing
                     #spaces
                     line = line.strip()
+                    #Remove aliases
+                    line = re.sub('state\s+\".*\"\s+as','state', line)
+                    #Accumulate all lines that end with \
                     if line.endswith('\\'):
                         accumulated_string += line[:-1]
                     else:
@@ -89,45 +92,81 @@ class PlantUMLCodeGeneration():
                         else:
                             uml_grouped.append(accumulated_string + line)
                             accumulated_string = ''
+
                 return uml_grouped
 
-    def ParseStateMachineAsDict(self):
+    def ParseStateMachineAsDict(self, uml_text):
         uml_params = self.StateMachineType()
+        line_num = 0
 
-        uml_text = self.GetUMLText(grouped=True)
-
-        for line in uml_text:
+        while line_num < len(uml_text):
+            line = uml_text[line_num]
             matchtransition = re.match('(\[\*\]|\w+)(?:|\s+)-->(?:|\s+)(\w+)(?:(?:|\s+)\:(.*))?',line)
-            matchstateaction = re.match('(?:state\s+)?\s+(\w+)(?:|\s+)(?:(?:|\s+)\:(.*))?',line)
+            matchstateaction = re.match('(?:state\s+)?(\w+)(?:|\s+)(?:(?:|\s+)\:(.*))?',line)
+            #matchsubmachine = re.match('(?:state\s+)?\s+(\w+)(?:|\s+)(?:(?:|\s+)\:(.*))?',line)
             if line.startswith('title'):
                 uml_params.title = line
-                continue
             elif matchtransition:
                 self.__AddTransition(uml_params, matchtransition)
             elif matchstateaction:
                 self.__AddStateActions(uml_params, matchstateaction)
+            #elif matchsubmachine:
+            #    pass
 
+            line_num += 1
 
+        #print('\n'.join(uml_text))
         for state in uml_params.states:
             print(state.upper(), uml_params.states[state].StringMe())
 
-        return uml_params
+        return uml_params, line_num
 
     def __AddTransition(self, uml_params, matchtransition):
         transition = self.TransitionType()
         state_origin = matchtransition.group(1)
         transition.destination = matchtransition.group(2)
-        transition.conditions = matchtransition.group(3)
+        conditions = matchtransition.group(3)
+        transition.conditions = conditions.replace('\\n','\n') if conditions else None
         #transition.actions = matchtransition.group(4)
         #Check if state exits, if not, create it
         if uml_params.states.get(state_origin) == None:
             uml_params.states[state_origin] = self.StateType()
         uml_params.states[state_origin].transitions.append(transition)
+        #Also, create destination state if it does not exist
+        if uml_params.states.get(transition.destination) == None:
+            uml_params.states[transition.destination] = self.StateType()
 
     def __AddStateActions(self, uml_params, matchstateaction):
-        state_origin = matchstateaction.group(1)
-        if uml_params.states.get(state_origin) == None:
-            uml_params.states[state_origin] = self.StateType()
+        state_name = matchstateaction.group(1)
+        actions = matchstateaction.group(2)
+        if uml_params.states.get(state_name) == None:
+            uml_params.states[state_name] = self.StateType()
+
+        #Get entry, exit and during
+        if actions:
+            #Do a regex split
+            action_matches = re.split(r'(entry\:|during\:|exit\:)', actions)
+
+            #The list will start with an empty string (or spaces) if it does not match entry
+            #any of the keywords. But if it starts with text it is a during
+            if action_matches[0].strip() != '':
+                uml_params.states[state_name].during = action_matches[0]
+            line_num = 1
+
+            while line_num < len(action_matches):
+                if action_matches[line_num] == 'entry:':
+                    uml_params.states[state_name].entry = action_matches[line_num + 1]
+                    line_num += 1
+                elif action_matches[line_num] == 'during:':
+                    uml_params.states[state_name].during = action_matches[line_num + 1]
+                    line_num += 1
+                elif action_matches[line_num] == 'exit:':
+                    uml_params.states[state_name].exit = action_matches[line_num + 1]
+                    line_num += 1
+                else:
+                    raise Exception('Action {} not recognized.'.format(action_matches[line_num]))
+                line_num += 1
+
 
     def GenerateFromTemplate(self, output_file, template_file, uml, uml_params):
         print('Environment:',os.path.dirname(template_file))
